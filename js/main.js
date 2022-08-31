5956808c8766a097f0dd2212c9a8eabb4e47ddd1
Nathalia <nathalia.miriam@hotmail.com>
2022-08-31 20:49:39 -0300

    client side form + buttons logic

--- js/main.js
+++ js/main.js
@@ -3,44 +3,72 @@ const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
 
 // função p fazer a busca dos pokemons na API 
-
     // fetch = utiliza o método GET do protocolo CRUD - busca os dados do pokemon na API ... async = p função assíncrona
 const fetchPokemon = async (pokemon) => {
-            
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`); // toLowerCase() = passa todos os caracteres do input p minusculo
     /* APIResponse = resposta da API na var(const) 
         await = p aguardar o término da chamada (do fetch), que é assíncrono
           assíncrono = algo que ñ acontece de maneira imediata, ou seja, devemos esperar 
     */
 
-    // pega a resposta da API e extrai em JSON q tbm é uma função assíncrona       
-    const dadosPokemon = await APIResponse.json();
+    if (APIResponse.status == 200) {
+        // pega a resposta da API e extrai em JSON q tbm é uma função assíncrona       
+        const dadosPokemon = await APIResponse.json();
 
-    return dadosPokemon;
+        return dadosPokemon;
+    }
 }
 
 // função p renderizar o pokemon no HTML
 const renderPokemon = async (pokemon) => {
 
-    const dados = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = "Loading..."; // p quando estiver buscando o pokemon
+    pokemonNumber.innerHTML = ""; // p ñ mostrar o nº do pokemon anterior quando estiver buscando o próximo
 
-    pokemonName.innerHTML = dados['name'];
-    pokemonNumber.innerHTML = dados['id'];
-    pokemonImage.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    const dados = await fetchPokemon(pokemon); // recebe os dados do pokemon a ser renderizado na tela
 
+    // checa se voltou algum dado de pokemon ou ñ (caso o pokemon ñ seja encontrado)
+   if(dados) {
+        pokemonName.innerHTML = dados['name'];
+        pokemonNumber.innerHTML = dados['id'];
+        pokemonImage.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = ''; // limpa o input após o clique do Enter
+    } else {
+        pokemonName.innerHTML = "Not Found :c"; // mensagem p quando ñ for encontrado um pokemon
+        pokemonNumber.innerHTML = ""; // limpa o nº quando o pokemon ñ for encontrado
+        pokemonImage.src = "";
+    }
 }
 
+// adiciona ao form o evento de submit, ou seja, ao enviar o form uma função será executada
 form.addEventListener('submit', function(event){
     
-    // preventDefault() = previne as configurações padrões do evento(event) p q possamos manusear o evento da maneira desejada
+    // por ser um form possui um comportamento padrão que precisará ser bloqueado ...
     event.preventDefault();
-    
-    console.log(event);
+    // o preventDefault previne as configurações padrões do evento(event) p q possamos manusear o evento da maneira desejada
+    // com isso já é possível resgatar o que for digitado no input
 
+    renderPokemon(input.value); // p renderizar o que for digitado no input 
 });
+
+buttonPrev.addEventListener("click", () => {
+    contador -= 1;
+    renderPokemon(contador.toString());
+});
+
+buttonNext.addEventListener("click", () => {
+    contador += 1;
+    renderPokemon(contador.toString())
+});
+
+let contador = 1;
+
+renderPokemon(contador.toString()); // aparecerá sempre o pokemon de nº 1 quando a pag. for renderizada/atualizada
 
 
 
